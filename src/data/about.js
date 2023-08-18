0fe4509b49f66530dbf9ee00c5f7be73ec262f86
support9-anaxee <support9@anaxee.com>
2023-08-19 00:03:42 +0530

    adding new files

---
+++ src/data/about.js
@@ -0,0 +1,67 @@
+const about = [
+  {
+    key: 1,
+    img: "https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/backend2.png?raw=true",
+    name: "Backend Developer",
+    description:
+      "Experienced Senior Full Stack Developer with a strong focus on backend development. With 3 years of expertise, I've led teams, optimized DevOps processes, and built robust systems. Specializing in Microservices, third-party API integration, and Docker for streamlined releases. Notably, I designed a Node.js architecture for a high-traffic app, enhancing performance. Skilled in Kubernetes, Firebase, and Azure Functions for seamless deployment and process optimization",
+    h1: "Technologies I use:",
+    h1Array: [
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "MySql",
+      "Firebase",
+      "azure Function",
+    ],
+    h2: "Dev Tools:",
+    h2Array: ["Postman", "Terminal", "Git", "Github", "Heroku"],
+  },
+  {
+    key: 2,
+    img: "https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/frontend2.png?raw=true",
+    name: "Frontend Developer",
+    description:
+      "I've been working for a year as full stack developer and know HTML, CSS, Sass, JavaScript, React.js, Redux, Webpack, and Next.js. I make user-friendly designs and enjoy teamwork. My goal is to create websites that users love.",
+    h1: "Technologies I use:",
+    h1Array: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "JavaScript",
+      "React.js",
+      "Redux",
+      "Webpack",
+      "Nextjs",
+    ],
+    h2: "Dev Tools:",
+    h2Array: [
+      "VSCode",
+      "Bootstrap",
+      "Terminal",
+      "Git",
+      "Github",
+      "CodeSandbox",
+      "Vercel",
+    ],
+  },
+  {
+    key: 3,
+    img: "https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/nitj-svg.png?raw=true",
+    name: "Devops",
+    description:
+      "I have two years of hands-on DevOps experience, coupled with my adeptness in cloud services from Azure, GCP, and AWS, along with a deep understanding of Kubernetes, have shaped me into a versatile professional capable of navigating the intricate interplay between development and operations. As I look ahead, I'm excited to further refine my skills, explore emerging technologies, and contribute to the ever-evolving landscape of modern software delivery..",
+    h1Array: ["Information Technology"],
+    h2: "Things I Do:",
+    h2Array: [
+      "Docker",
+      "Linux",
+      "Automation",
+      "Azure devops",
+      "Kubernetes",
+      "CI/CD",
+    ],
+  },
+];
+
+export default about;
---
+++ src/data/education.js
@@ -0,0 +1,20 @@
+const education = [
+  {
+    key: 1,
+    img: "https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/education/nitj.png?raw=true",
+    name: "Shri Govindram Seksaria Institute of Technology And Science",
+    duration: "2018 - 2021",
+    education: "Information Technology",
+    degree: "Master of Computer Applications",
+  },
+  {
+    key: 2,
+    img: "https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/education/msp.png?raw=true",
+    name: "Jiwaji University Gwalior",
+    duration: "2014 - 2017",
+    education: "Computer Application",
+    degree: "Bachelor of Computer Application",
+  },
+];
+
+export default education;
